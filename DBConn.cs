c11d4c76c8d6d74818c81ded9382e6c06039b1f7
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2023-10-02 14:02:17 +0300

    Initial commit

---
+++ DBConn.cs
@@ -0,0 +1,32 @@
+﻿using MySqlConnector;
+using System.Diagnostics;
+namespace cherepashki
+{
+    internal static class DBConn
+    {
+        static MySqlConnection connection;
+        public static MySqlConnection StartConnection(string auditoriya)
+        {
+            const string ip_112 = "Server=192.168.56.101;User ID=student;Password=1234567890;Database=test";
+            const string ip_306 = "Server=192.168.56.102;User ID=student;Password=1234567890;Database=test";
+            switch (auditoriya)
+            {
+                case "112":
+                    connection = new MySqlConnection(ip_112);
+                    break;
+                case "306":
+                    connection = new MySqlConnection(ip_306);
+                    break;
+                default:
+                    connection = null;
+                    break;
+            }
+            if (connection != null)
+            {
+                connection.Open();
+                Debug.Print(connection.ServerVersion);
+            }
+            return connection;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using MySqlConnector;
+using System;
+using System.Windows.Forms;
+
+namespace cherepashki
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DBConn.StartConnection(comboBox1.Text);
+        }
+    }
+}
